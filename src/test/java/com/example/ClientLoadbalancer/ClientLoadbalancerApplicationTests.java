9006674e0f9f44e42166eefe8c1bb63a3bfe3b42
shamim <shamimahmad586@gmail.com>
2022-09-06 14:08:22 +0530

    Client Loadbalancer first commit

---
+++ src/test/java/com/example/ClientLoadbalancer/ClientLoadbalancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ClientLoadbalancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientLoadbalancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
